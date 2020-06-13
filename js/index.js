009e4d7f18d5b9c7b04c45ac9d3361935cf58773
Krzysztof Kozak <hithard5@gmail.com>
2020-06-13 19:23:11 +0200

    added scss modules, added js script to close the mobile menu on window resize

--- js/index.js
+++ js/index.js
@@ -1,10 +1,17 @@
 const init = () => {
-  console.log("DOM succesfuly parsed and loaded");
+  console.log('DOM succesfuly parsed and loaded');
 
-  const button = document.querySelector(".button-js");
-  const menu = document.querySelector(".menu-js");
-  const toggleMenu = () => menu.classList.toggle("menu--open");
+  const button = document.querySelector('.button--js');
+  const menu = document.querySelector('.menu-js');
+  const toggleMenu = () => menu.classList.toggle('menu--open');
 
-  button.addEventListener("click", toggleMenu);
+  window.addEventListener('resize', () => {
+    if (window.innerWidth >= 768 && menu.classList.contains('menu--open')) {
+      menu.classList.remove('menu--open');
+      console.log('window resized to >= 768 px, menu closed!');
+    }
+  });
+
+  button.addEventListener('click', toggleMenu);
 };
-document.addEventListener("DOMContentLoaded", init);
+document.addEventListener('DOMContentLoaded', init);
